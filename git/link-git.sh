20e7ccee24eb906967c42dec3bde2a021becc28a
ttak0422 <bgdaewalkman@gmail.com>
2019-12-13 14:42:41 +0900

    :shower: 不要なechoを削除

--- git/link-git.sh
+++ git/link-git.sh
@@ -2,11 +2,9 @@
 
 readonly SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
-echo $SCRIPT_DIR
 cd $SCRIPT_DIR
 
 for file in .??*; do 
-  echo $file
   [ -e ${HOME}/${file} ] && continue
   ln -snfv ${SCRIPT_DIR}/${file} ${HOME}/${file}
 done
