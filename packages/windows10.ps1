e2720f9897be3d407a3bc124dcddf4f257cfd376
ttak0422 <bgdaewalkman@gmail.com>
2020-01-05 00:40:37 +0900

    :+1: add flutter

--- packages/windows10.ps1
+++ packages/windows10.ps1
@@ -1,32 +1,24 @@
-# 以後，インストールに関する認証を省略できるようにする．
 function EnableAllowConfirmation()
 {
     choco feature enable --name=allowGlobalConfirmation
 }
 
-# 以後，インストールに関する認証を省略できないようにする．
 function DisableAllowConfirmation()
 {
     choco feature disable --name=allowGlobalConfirmation
 }
 
-# ブラウザ関連のアプリのhインストール
 function InstallBrowserApps()
 {
-    <# Chromeのインストールを行う
-       --limit-outputはターミナルに出力される情報を必要なものに制限させるオプションです． #>
     choco install GoogleChrome --limit-output
-    # Chromeのバージョン更新をchocolateyで行わないように指示するコマンドです．
     choco pin add --name GoogleChrome --limit-output
 }
 
-# Fontのインストール
 function InstallFonts()
 {
     choco install firacode --limit-output
 }
 
-# 開発ツールのインストール
 function InstallDevTools()
 {
     choco install git --limit-output
@@ -45,18 +37,29 @@ function InstallDevTools()
     choco install dotnetcore-sdk --limit-output
 }
 
-<# EntryPoint
-   管理者権限で実行されていない場合終了する #>
 if (-not(([Security.Principal.WindowsPrincipal] `
     [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
     [Security.Principal.WindowsBuiltInRole] "Administrator"`
     ))) {
-    echo "管理者権限で実行する必要があります"
+    Write-Output "Must be run with authentication rights"
     exit
 }
 
 EnableAllowConfirmation
 InstallBrowserApps
-InstallFonts
+
 InstallDevTools
 DisableAllowConfirmation
+
+function InstallFlutter()
+{
+    if(!(Test-Path $HOME\flutter))
+    {
+        Write-Output "Install Flutter"
+        git clone https://github.com/flutter/flutter.git $HOME\flutter
+    } else {
+        Write-Output "Already installed flutter"
+    }
+}
+
+InstallFlutter
