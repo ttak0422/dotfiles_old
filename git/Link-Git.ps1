f2747051007aab9da1562d85811c56c911044d2c
ttak0422 <bgdaewalkman@gmail.com>
2020-01-16 12:59:03 +0900

    :+1: add gitignore_global

--- git/Link-Git.ps1
+++ git/Link-Git.ps1
@@ -1,2 +1,3 @@
 New-Item -Type SymbolicLink $HOME\.gitconfig -Value $PSScriptRoot\.gitconfig
 New-Item -Type SymbolicLink $HOME\.commit_template -Value $PSScriptRoot\.commit_template
+New-Item -Type SymbolicLink $HOME\.gitignore_global $PSScriptRoot\.gitignore_global
