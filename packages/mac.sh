b348a12f6e443157458884d76dd2d82d3ffa483a
tak <tak@taknoMac-mini.local>
2020-02-05 06:44:01 +0900

    :+1: add package for macos

---
+++ packages/mac.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+declare -r SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+cd $SCRIPT_DIR
+
+install_zinit() {
+    if [[ ! -d $HOME/.zinit ]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+    fi
+}
+
+install_zinit
+
+brew update
+
+declare -ar brew_cask_packages=(
+    'visual-studio-code'
+    'jetbrains-toolbox'
+    'dotnet-sdk'
+)
+
+declare -ar brew_packages=(
+)
+
+for p in ${brew_cask_packages[@]}; do
+    brew cask install $p
+done
+
+for p in ${brew_packages[@]}; do 
+    brew install $p 
+done
