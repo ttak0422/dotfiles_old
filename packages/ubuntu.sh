e52d00e8fc9b0a7a291da414536aa199dadd9b2e
ttak0422 <bgdaewalkman@gmail.com>
2020-04-04 23:57:16 +0900

    :construction: WIP

--- packages/ubuntu.sh
+++ packages/ubuntu.sh
@@ -14,6 +14,33 @@ install_zinit() {
   fi
 }
 
+install_docker() {
+  if [[ ! -e /usr/bin/docker ]]; then
+    sudo apt-get -y install \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg-agent \
+      software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) \
+      stable"
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+    sudo usermod -aG docker $USER
+    sudo service docker start
+  fi
+}
+
+install_compose() {
+  if [[ ! -e /usr/local/bin/docker-compose ]]; then 
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+  fi
+}
+
 install_python() {
   # pyenv
   if [[ ! -d $HOME/.pyenv ]]; then
@@ -104,6 +131,8 @@ install_powerline
 install_python
 install_jvm
 install_tpm
+install_docker
+install_compose
 
 sudo apt-get -y autoremove
 source ../zsh/.zshenv
