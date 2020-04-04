07474d0aea4e902ed83c1ba7c12db3db9b5b573c
ttak0422 <bgdaewalkman@gmail.com>
2020-04-04 10:52:41 +0900

    :construction: use Docker for WSL2
    今の段階ではWSL2状のDevcontainerを開くことができないのでWin上にクローンする必要がある。
    地味につらい。

--- packages/ubuntu.sh
+++ packages/ubuntu.sh
@@ -14,33 +14,6 @@ install_zinit() {
   fi
 }
 
-install_docker() {
-  if [[ ! -e /usr/bin/docker ]]; then
-    sudo apt-get -y install \
-      apt-transport-https \
-      ca-certificates \
-      curl \
-      gnupg-agent \
-      software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) \
-      stable"
-    sudo apt-get -y update
-    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-    sudo usermod -aG docker $USER
-    sudo service docker start
-  fi
-}
-
-install_compose() {
-  if [[ ! -e /usr/local/bin/docker-compose ]]; then 
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-  fi
-}
-
 install_python() {
   # pyenv
   if [[ ! -d $HOME/.pyenv ]]; then
