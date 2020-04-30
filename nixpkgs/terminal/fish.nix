98bd1893c43f62e187b2a5e50eb6001d8b3e8e5f
ttak0422 <bgdaewalkman@gmail.com>
2020-04-30 20:52:01 +0900

    :bug: fix alias

--- nixpkgs/terminal/fish.nix
+++ nixpkgs/terminal/fish.nix
@@ -1,17 +1,20 @@
 { config, pkgs, lib, ... }:
 
 {
+  home.packages = with pkgs; [
+    powerline-go
+  ];
   programs.fish = {
     enable = true;
     functions = {
-        fish_prompt = "eval /home/tak/.nix-profile/bin/powerline-go -error $status -shell bare -modules venv,user,ssh,cwd,perms,git,hg,jobs,exit -newline";      
+        fish_prompt = "eval $HOME/.nix-profile/bin/powerline-go -error $status -shell bare -modules venv,user,ssh,cwd,perms,git,hg,jobs,exit -newline";      
     };
     shellAbbrs = {
       "d" = "docker";
       "dc" = "docker-compose";      
     };
     shellAliases = {
-      "g"  = ''cd (ghq root)'/'(ghq list | peco))'';
+      "g"  = ''cd (ghq root)'/'(ghq list | peco)'';
       "gh" = ''hub browse (ghq list | peco | cut -d "/" -f 2,3)'';
     };
   };
