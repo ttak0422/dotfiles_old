ed542a7acb6d8e2c11f85ff0d96354c9d45ae3f4
ttak0422 <bgdaewalkman@gmail.com>
2020-07-26 15:31:42 +0900

    :+1: utils

---
+++ utils/default.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, lib, ... }:
+{
+  nixpkgs.config.allowUnfree = true;
+  time.timeZone = "Asia/Tokyo";
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  environment.pathsToLink = [ "/libexec" ];
+}
