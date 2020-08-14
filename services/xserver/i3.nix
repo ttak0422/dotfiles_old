78c4514fdced44834090af64c8ead1de80068932
ttak0422 <bgdaewalkman@gmail.com>
2020-08-15 02:25:51 +0900

    :bug: fix i3 logout

--- services/xserver/i3.nix
+++ services/xserver/i3.nix
@@ -128,7 +128,7 @@ let
     set $mode_system System (l) lock, (e) logout, (s) suspend, (h) hibernate, (r) reboot, (Shift+s) shutdown
     mode "$mode_system" {
       bindsym l exec --no-startup-id i3exit lock, mode "default"
-      bindsym e exec --no-startup-id i3exit logout, mode "default"
+      bindsym e exec --no-startup-id pkill x, mode "default"
       bindsym s exec --no-startup-id systemctl suspend, mode "default"
       bindsym h exec --no-startup-id i3exit hibernate, mode "default"
       bindsym r exec --no-startup-id i3exit reboot, mode "default"
