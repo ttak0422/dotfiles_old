5034f6a9a67686f7693cee10149ef4fb3ab59aaf
ttak0422 <bgdaewalkman@gmail.com>
2020-08-25 15:12:40 +0900

    :recycle: picom

--- services/picom.nix
+++ services/picom.nix
@@ -2,22 +2,15 @@
   services.picom = {
     enable = true;
     shadow = false;
-    # vSync = "opengl-swc";
     backend = "glx";
-    # todo
-    opacityRules =
-      [ "100:class_g = 'URxvt' && focused" "80:class_g = 'URxvt' && !focused" ];
     activeOpacity = "1.0";
-    inactiveOpacity = "0.9";
+    inactiveOpacity = "1.0";
     wintypes = {
       tooltip = {
-        fade = true;
+        fade = false;
         shadow = false;
-        opacity = "0.8";
       };
     };
-    fadeDelta = 5;
-    fadeSteps = [ "0.03" "0.03" ];
-    fade = true;
+    fade = false;
   };
 }
