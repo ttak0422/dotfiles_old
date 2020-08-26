1e8aa24a1a3bb40fae2d0bae929628b1cb36e19c
ttak0422 <bgdaewalkman@gmail.com>
2020-08-26 23:02:18 +0900

    :construction: polybar

--- home/nixos/polybar.nix
+++ home/nixos/polybar.nix
@@ -14,6 +14,7 @@ let
     purple = "#c678dd";
     teal = "#56b6c2";
   };
+  padding = 2;
 in {
   services.polybar = {
     enable = true;
@@ -32,11 +33,11 @@ in {
         offset-x = "0%";
         ffset-y = "0%";
         background = colors.mono0;
-        horeground = colors.green;
+        foreground = colors.green;
         radius = 0;
         modules-left = "i3";
-        modules-center = "date";
-        modules-right = "battery";
+        modules-center = "title";
+        modules-right = "alsa backlight battery date";
         line-size = 3;
         line-color = "#f00";
 
@@ -49,10 +50,10 @@ in {
         module-margin-left = 0;
         module-margin-right = 0;
 
-        font-0 = "FSource Han Code JP H;0";
-        font-1 = "FontAwesome5Free:style=Solid;4";
-        font-2 = "FontAwesome5Free:style=Regular;4";
-        font-3 = "FontAwesome5Brands:style=Regular;4";
+        font-0 = "FontAwesome5Free:style=Solid;4";
+        font-1 = "FontAwesome5Free:style=Regular;4";
+        font-2 = "FontAwesome5Brands:style=Regular;4";
+        font-3 = "FSource Han Code JP H;0";
 
       };
       "module/i3" = {
@@ -64,41 +65,119 @@ in {
         label-focused = "%index%";
         label-focused-background = colors.green;
         label-focused-foreground = colors.mono0;
-        label-focused-padding = 2;
+        label-focused-padding = padding;
 
         # unfocused = Inactive workspace on any monitor
         label-unfocused = "%index%";
         label-unfocused-background = colors.mono4;
         label-unfocused-foreground = colors.mono2;
-        label-unfocused-padding = 2;
+        label-unfocused-padding = padding;
 
         # visible = Active workspace on unfocused monitor
         label-visible = "%index%";
         label-visible-background = colors.mono4;
         label-visible-foreground = colors.mono2;
-        label-visible-padding = 2;
+        label-visible-padding = padding;
 
         # urgent = Workspace with urgency hint set
         label-urgent = "%index%";
         label-urgent-background = colors.red;
         label-urgent-foreground = colors.mono0;
-        label-urgent-padding = 2;
+        label-urgent-padding = padding;
       };
       "module/date" = {
         type = "internal/date";
         internal = 5;
-        date = "%Y-%m-%d";
+        date = "%m-%d";
         time = "%H:%M";
         label = "%date% %time%";
+        format-padding = padding;
+        format-background = colors.green;
+        format-foreground = colors.mono0;
+
+      };
+      "module/alsa" = {
+        type = "internal/alsa";
+
+        format-volume = "<ramp-volume> <label-volume>";
+        format-volume-background = colors.mono4;
+        format-volume-foreground = colors.mono2;
+        format-volume-padding = padding;
+        format-muted-background = colors.mono4;
+        format-muted-foreground = colors.mono2;
+
+        label-volume = "%percentage%%";
+        label-muted = " Muted";
+        label-muted-padding = padding;
+        #label-muted-foreground = ${color.fg-alt}
+
+        ramp-volume-0 = "";
+        ramp-volume-1 = "";
+        ramp-volume-2 = "";
+        ramp-volume-3 = "";
+      };
+
+      "module/backlight" = {
+        type = "internal/xbacklight";
+
+        card = "intel_backlight";
+        format = "<ramp>";
+
+        ramp-0 = "";
+        ramp-1 = "";
+        ramp-2 = "";
+        ramp-3 = "";
+        ramp-4 = "";
+      };
+      "module/title" = {
+        type = "internal/xwindow";
+
+        format = "<label>";
+
+        label = "%title%";
+        label-maxlen = 25;
       };
       "module/battery" = {
-        type = "custom/script";
-        exec =
-          "/run/current-system/sw/bin/bash /run/current-system/sw/bin/myPolyBarBattery";
-        interval = 60;
-        format-padding = 1;
-        format-background = colors.mono4;
-        format-foreground = colors.mono2;
+        type = "internal/battery";
+
+        full-at = 99;
+
+        battery = "BAT0";
+        adapter = "AC";
+
+        poll-interval = 2;
+        time-format = "%H:%M";
+
+        format-charging = "<animation-charging> <label-charging>";
+        format-discharging = "<ramp-capacity> <label-discharging>";
+        format-full = "<ramp-capacity> <label-full>";
+
+        format-charging-background = colors.mono4;
+        format-charging-foreground = colors.mono2;
+        format-charging-padding = padding;
+        format-discharging-background = colors.mono4;
+        format-discharging-foreground = colors.mono2;
+        format-discharging-padding = padding;
+        format-full-foreground = colors.mono4;
+        format-full-background = colors.mono2;
+        format-full-padding = padding;
+        label-charging = "%percentage%%";
+        label-discharging = "%percentage%%";
+        label-full = "Charged";
+
+        ramp-capacity-0 = "";
+        ramp-capacity-1 = "";
+        ramp-capacity-2 = "";
+        ramp-capacity-3 = "";
+        ramp-capacity-4 = "";
+
+        animation-charging-0 = "";
+        animation-charging-1 = "";
+        animation-charging-2 = "";
+        animation-charging-3 = "";
+        animation-charging-4 = "";
+
+        animation-charging-framerate = 750;
       };
     };
   };
