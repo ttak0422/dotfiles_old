7d7961d97051869eaafdad3d08a33608e82a129c
ttak0422 <bgdaewalkman@gmail.com>
2020-09-06 11:40:44 +0900

    :+1: add unstable unityhub

--- gui/default.nix
+++ gui/default.nix
@@ -1,11 +1,16 @@
-{ config, pkgs, lib, ... }: {
-  environment.systemPackages = with pkgs; [
-    firefox
-    google-chrome
-    vscode
-    termite
-    slack
-    teams
-    typora
+{ config, pkgs, lib, ... }:
+let
+  sources = import ./../nix/sources.nix;
+  pkgsUnstable = import sources.nixpkgs-unstable { config.allowUnfree = true; };
+in {
+  environment.systemPackages = [
+    pkgs.firefox
+    pkgs.google-chrome
+    pkgs.vscode
+    pkgs.termite
+    pkgs.slack
+    pkgs.teams
+    pkgs.typora
+    pkgsUnstable.unityhub
   ];
 }
