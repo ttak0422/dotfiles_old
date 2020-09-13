8af4b2a32049289de7008159dc25d2ceaaf99023
ttak0422 <bgdaewalkman@gmail.com>
2020-09-13 10:32:27 +0900

    :recycle: update bashrc

--- home/generic/bash.nix
+++ home/generic/bash.nix
@@ -1,1 +1,9 @@
-{ config, pkgs, lib, ... }: { programs.bash = { enable = true; }; }
+{ config, pkgs, lib, ... }: {
+  programs.bash = { enable = true; };
+
+  programs.bash.bashrcExtra = ''
+    if type "fish" > /dev/null 2>&1; then
+      exec fish
+    fi
+  '';
+}
