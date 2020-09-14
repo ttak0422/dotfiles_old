9f74cacbd6914c74a2d6dc4e666e41f87c1a5ec0
ttak0422 <bgdaewalkman@gmail.com>
2020-09-14 21:08:11 +0900

    :arrow_up_small: 20.09

--- home/nix-home.nix
+++ home/nix-home.nix
@@ -1,5 +1,5 @@
 { config, pkgs, lib, ... }: {
   programs.home-manager.enable = true;
-  home.stateVersion = "20.03";
+  home.stateVersion = "20.09";
   imports = [ ./generic ./nixos ./nixos/polybar.nix ];
 }
