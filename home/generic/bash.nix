fe2edba1f8f1ceb9389d46357cce597c27d27424
ttak0422 <bgdaewalkman@gmail.com>
2020-09-19 21:31:28 +0900

    :construction: update bashrc
    exec fishをしているとnix selectorが正しく動作しないことがある．

--- home/generic/bash.nix
+++ home/generic/bash.nix
@@ -1,9 +1,9 @@
 { config, pkgs, lib, ... }: {
   programs.bash = { enable = true; };
 
-  programs.bash.bashrcExtra = ''
-    if type "fish" > /dev/null 2>&1; then
-      exec fish
-    fi
-  '';
+#  programs.bash.bashrcExtra = ''
+#    if type "fish" > /dev/null 2>&1; then
+#      exec fish
+#    fi
+#  '';
 }
