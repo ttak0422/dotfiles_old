f82678b36731cd600c4bdf411c74ffacdb07923f
ttak0422 <bgdaewalkman@gmail.com>
2020-09-26 09:10:33 +0900

    :construction: update bashrc
    nix-environment selectorと相性が良くないかも

--- home/generic/bash.nix
+++ home/generic/bash.nix
@@ -1,9 +1,1 @@
-{ config, pkgs, lib, ... }: {
-  programs.bash = { enable = true; };
-
-#  programs.bash.bashrcExtra = ''
-#    if type "fish" > /dev/null 2>&1; then
-#      exec fish
-#    fi
-#  '';
-}
+{ config, pkgs, lib, ... }: { programs.bash = { enable = true; }; }
