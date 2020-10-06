c78225dc19606037b98c8b0ebd5642868f09d8cc
ttak0422 <bgdaewalkman@gmail.com>
2020-10-06 20:46:00 +0900

    :+1: update zshrc
    nix-darwin

--- home/generic/zsh.nix
+++ home/generic/zsh.nix
@@ -3,6 +3,7 @@
     enable = true;
     initExtra = ''
       test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
+      test -e "/etc/static/bashrc" && source "/etc/static/bashrc"
     '';
     profileExtra = ''
       [[ -e ~/.profile ]] && emulate sh -c 'source ~/.profile'
