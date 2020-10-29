b0d042270d0d1e37f91cfe73177ee4e077e9c264
ttak0422 <bgdaewalkman@gmail.com>
2020-10-29 17:41:50 +0900

    :+1: add python 3

--- home/generic/development/default.nix
+++ home/generic/development/default.nix
@@ -1,3 +1,3 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ coreutils dotnet-sdk_3 nodejs yarn rustup adoptopenjdk-jre-openj9-bin-13 ruby_2_7 ];
+  home.packages = with pkgs; [ coreutils dotnet-sdk_3 nodejs yarn rustup adoptopenjdk-jre-openj9-bin-13 ruby_2_7 python3 ];
 }
