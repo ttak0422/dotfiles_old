75011c823cc7b96cd3309c294c43ff5dd21a3f2a
ttak0422 <bgdaewalkman@gmail.com>
2020-11-08 16:30:29 +0900

    :+1: bash powerline

--- home/generic/bash.nix
+++ home/generic/bash.nix
@@ -1,1 +1,20 @@
-{ config, pkgs, lib, ... }: { programs.bash = { enable = true; }; }
+{ config, pkgs, lib, ... }: 
+{ 
+  home.packages = with pkgs; [ powerline-go ];
+  programs.bash = { 
+    enable = true; 
+    bashrcExtra = ''
+      function _update_ps1_a() {
+        PS1="$($HOME/.nix-profile/bin/powerline-go -error $?)"
+      }
+      function _update_ps1_b() {
+        PS1="$(/run/current-system/sw/bin/powerline-go -error $?)"
+      }
+      if [ -f "$HOME/.nix-profile/bin/powerline-go" ]; then
+        PROMPT_COMMAND="_update_ps1_a; $PROMPT_COMMAND"
+      elif [ -f "/run/current-system/sw/bin/powerline-go" ]; then
+        PROMPT_COMMAND="_update_ps1_b; $PROMPT_COMMAND"
+      fi
+    '';
+  }; 
+}
