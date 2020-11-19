7dc89f80cd6480e95f5a32cf486fb68aa40ad37e
ttak0422 <bgdaewalkman@gmail.com>
2020-11-20 01:45:33 +0900

    :+1: some modified zshrc

--- nix/home/generic/zsh.nix
+++ nix/home/generic/zsh.nix
@@ -6,9 +6,17 @@
       test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
       # test -e "/etc/static/bashrc" && source "/etc/static/bashrc"
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-    '';
-    profileExtra = ''
+      # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
       [[ -e ~/.profile ]] && emulate sh -c 'source ~/.profile'
+    '';
+    shellAliases = {
+      "g" = "cd (ghq root)'/'(ghq list | peco)";
+      "gh" = ''hub browse (ghq list | peco | cut -d "/" -f 2,3)'';
+      "gg" = "ghq get";
+      "c" = "xclip -selection clipboard";
+    };
+    profileExtra = ''
     '';
   };
 }
