e5cf5df46c5be5cb85074dddc190e9af65f77c5e
ttak0422 <bgdaewalkman@gmail.com>
2020-11-20 01:34:47 +0900

    :+1: powerlevel10k

--- nix/home/generic/zsh.nix
+++ nix/home/generic/zsh.nix
@@ -1,9 +1,11 @@
 { config, pkgs, lib, ... }: {
+  home.packages = with pkgs; [ zsh-powerlevel10k ];
   programs.zsh = {
     enable = true;
     initExtra = ''
       test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
-      test -e "/etc/static/bashrc" && source "/etc/static/bashrc"
+      # test -e "/etc/static/bashrc" && source "/etc/static/bashrc"
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
     '';
     profileExtra = ''
       [[ -e ~/.profile ]] && emulate sh -c 'source ~/.profile'
