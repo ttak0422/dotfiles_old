72efb07ff53eebacbd234703f2dd47cef7625207
ttak0422 <bgdaewalkman@gmail.com>
2020-11-25 02:18:27 +0900

    :recycle: js

--- nix/home/generic/development/default.nix
+++ nix/home/generic/development/default.nix
@@ -1,3 +1,13 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ coreutils dotnet-sdk_3 nodejs yarn rustup adoptopenjdk-jre-openj9-bin-13 ruby_2_7 python3 ];
+  imports = [
+    ./js.nix
+  ];
+  home.packages = with pkgs; [ 
+    coreutils 
+    dotnet-sdk_3 
+    rustup 
+    adoptopenjdk-jre-openj9-bin-13 
+    ruby_2_7 
+    python3
+  ];
 }
