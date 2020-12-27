a2815f42395a5b644ec1b7038eb6022343c8d3ea
ttak0422 <bgdaewalkman@gmail.com>
2020-12-27 19:22:16 +0900

    :construction: update .tigrc

--- nix/home/generic/git/default.nix
+++ nix/home/generic/git/default.nix
@@ -1,4 +1,4 @@
 { config, pkgs, lib, ... }: {
   home.packages = with pkgs; [ git tig ghq gitAndTools.hub ];
-  imports = [ ./config.nix ./template.nix ./ignore.nix ];
+  imports = [ ./config.nix ./template.nix ./ignore.nix ./tig.nix ];
 }
