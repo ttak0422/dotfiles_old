7fecb9a0b7448ae6608b3c166be41b87fdb9d2cb
ttak0422 <bgdaewalkman@gmail.com>
2020-12-31 16:17:10 +0900

    :construction: [ghq]

--- nix/home/generic/development/go.nix
+++ nix/home/generic/development/go.nix
@@ -1,11 +1,16 @@
-{ config, pkgs, lib, ... }: 
-let 
-  PATH="${builtins.getEnv "HOME"}/go";
+{ config, pkgs, lib, ... }:
+let
+  go-path = "${builtins.getEnv "HOME"}/go";
+  ghq-path = "${builtins.getEnv "HOME"}/ghq";
 in {
-  home.packages = with pkgs; [
-    go
-  ];
+  home.packages = with pkgs; [ go ];
   home.file.".bashrc".text = lib.mkAfter ''
-    export GOPATH=${PATH}
+    export GOPATH=${go-path}:${ghq-path}
+    export GO111MODULE=on
+  '';
+  home.file.".config/git/config".text = lib.mkAfter ''
+    [ghq]
+      root = ${go-path}/src
+      root = ${ghq-path}/src
   '';
 }
