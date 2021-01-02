00936611fb49a07bd4d964ae4a0d2b9803f8f87c
ttak0422 <bgdaewalkman@gmail.com>
2021-01-02 14:11:53 +0900

    coreutils >> coreutils-full

--- nix/home/generic/development/default.nix
+++ nix/home/generic/development/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   imports = [ ./js.nix ./python.nix ./go.nix ];
   home.packages = with pkgs; [
-    coreutils
+    coreutils-full
     dotnet-sdk_3
     rustup
     adoptopenjdk-jre-openj9-bin-13
