3a6c4e0faad27852adbc571f27e2c28575fb664a
ttak0422 <bgdaewalkman@gmail.com>
2021-01-04 02:48:06 +0900

    :recycle:

--- nix/home/generic/default.nix
+++ nix/home/generic/default.nix
@@ -16,6 +16,7 @@
   #]);
   home.packages = with pkgs;
     [
+      coreutils-full
       xclip
       jq
       wget
--- nix/home/generic/development/default.nix
+++ nix/home/generic/development/default.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }: {
   imports = [ ./js.nix ./python.nix ./go.nix ];
   home.packages = with pkgs; [
-    coreutils-full
     dotnet-sdk_3
     rustup
     adoptopenjdk-jre-openj9-bin-13
