51783a1285e230e0a7e643613ae7bcdef113adbc
ttak0422 <bgdaewalkman@gmail.com>
2021-01-07 16:06:53 +0900

    change style

--- nix/home/generic/tmux.nix
+++ nix/home/generic/tmux.nix
@@ -92,7 +92,7 @@ let
     set -g message-style fg=green,reverse,bg=default
 
     # left
-    set -g status-left "#[fg=white,bg=blue]#{?client_prefix,#[bg=yellow],} Session: #S #[default]#[fg=blue]#{?client_prefix,#[fg=yellow],}"
+    set -g status-left "#[fg=white,bg=blue]#{?client_prefix,#[bg=yellow],} Session: #S #[default]#[fg=blue]#{?client_prefix,#[fg=yellow],}"
 
     # center
     set-option -g status-justify "centre"
@@ -100,7 +100,7 @@ let
     set-window-option -g window-status-current-format "#[fg=black,bg=white] #I: #W #[default]"
 
     # right
-    set -g status-right "#[fg=red]#[default]#[fg=white,bg=red] %H:%M #[default]"
+    set -g status-right "#[fg=red]#[default]#[fg=white,bg=red] %H:%M #[default]"
 
     # default shell
     set-option -g default-shell "${defaultShell}"
