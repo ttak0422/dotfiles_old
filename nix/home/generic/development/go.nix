fc3ea4c7d16c2aa5b84f812193bbcc628e5dd6d0
ttak0422 <bgdaewalkman@gmail.com>
2021-01-10 22:55:53 +0900

    add path $GOPATH/bin

--- nix/home/generic/development/go.nix
+++ nix/home/generic/development/go.nix
@@ -2,12 +2,15 @@
 let
   go-path = "${builtins.getEnv "HOME"}/go";
   ghq-path = "${builtins.getEnv "HOME"}/ghq";
-in {
-  home.packages = with pkgs; [ go ];
-  home.file.".bashrc".text = lib.mkAfter ''
+  rc = ''
     export GOPATH=${go-path}:${ghq-path}
     export GO111MODULE=on
+    PATH=$PATH:${go-path}/bin
   '';
+in {
+  home.packages = with pkgs; [ go ];
+  home.file.".bashrc".text = lib.mkAfter rc;
+  home.file.".zshrc".text = lib.mkAfter rc;
   home.file.".config/git/config".text = lib.mkAfter ''
     [ghq]
       root = ${go-path}/src
