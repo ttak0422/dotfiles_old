901eee2184fe50c8f6588becbc9ee9948bd8ff7d
ttak0422 <bgdaewalkman@gmail.com>
2021-01-13 03:19:42 +0900

    :recycle:

--- nix/home/generic/development/go.nix
+++ nix/home/generic/development/go.nix
@@ -2,18 +2,18 @@
 let
   go-path = "${builtins.getEnv "HOME"}/go";
   ghq-path = "${builtins.getEnv "HOME"}/ghq";
-  rc = ''
-    export GOPATH=${go-path}:${ghq-path}
-    export GO111MODULE=on
-    PATH=$PATH:${go-path}/bin
-  '';
 in {
-  home.packages = with pkgs; [ go ];
-  home.file.".bashrc".text = lib.mkAfter rc;
-  home.file.".zshrc".text = lib.mkAfter rc;
-  home.file.".config/git/config".text = lib.mkAfter ''
-    [ghq]
-      root = ${go-path}/src
-      root = ${ghq-path}/src
-  '';
+  home = {
+    packages = with pkgs; [ go ];
+    sessionVariables = {
+      PATH = "$PATH:${go-path}/bin";
+      GOPATH = "${go-path}:${ghq-path}";
+      GO111MODULE = "on";
+    };
+    file.".config/git/config".text = lib.mkAfter ''
+      [ghq]
+        root = ${go-path}/src
+        root = ${ghq-path}/src
+    '';
+  };
 }
