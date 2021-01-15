c2b6d2fa509469eb6e1bb98b24b2e256a2c8cfca
ttak0422 <bgdaewalkman@gmail.com>
2021-01-16 08:59:57 +0900

    :checkered_flag: btm does not work

--- nix/home/generic/shell/shared.nix
+++ nix/home/generic/shell/shared.nix
@@ -6,7 +6,7 @@
       "cd $(ghq root)/$(ghq list | peco)";
     "gg" = "ghq get";
     ".." = "cd ..";
-    "top" = "btm";
+    # "top" = "btm";
     "ps" = "procs";
     "vi" = "${pkgs.vim}/bin/vim";
     "vim" = "nvim";
