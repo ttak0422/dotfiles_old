27f67bbb90eabdffaa722c0fd2b433003da0be19
ttak0422 <bgdaewalkman@gmail.com>
2021-02-08 13:41:24 +0900

    add lorri

--- nix/darwin/desktop.nix
+++ nix/darwin/desktop.nix
@@ -1,5 +1,5 @@
 { config, pkgs, lib, ... }: {
-  imports = [ ./window-manager ];
+  imports = [ ./tools ./window-manager ];
   nix.nixPath = [
     "darwin-config=$HOME/dotfiles/nix/darwin/desktop/configuration.nix"
     "darwin=$HOME/.nix-defexpr/channels/darwin"
