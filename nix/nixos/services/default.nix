1fefa610ced68262892dc5fde33e1dad98ce14ce
ttak0422 <bgdaewalkman@gmail.com>
2021-02-16 09:43:18 +0000

    add key関連

--- nix/nixos/services/default.nix
+++ nix/nixos/services/default.nix
@@ -1,4 +1,6 @@
 { config, pkgs, lib, ... }: {
   imports = [ ./xserver/i3-xfce.nix ./picom.nix ];
+  environment.systemPackages = with pkgs; [ gnome3.seahorse gnome3.libsecret ];
   services.xserver.enable = true;
+  services.gnome3.gnome-keyring.enable = true;
 }
