ed39180823f6519a4f815b725c2947ad8d0e9d61
ttak0422 <bgdaewalkman@gmail.com>
2021-03-01 01:04:25 +0900

    use nightly nvim

--- nix/home/tools/vim.nix
+++ nix/home/tools/vim.nix
@@ -1,6 +1,16 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+
+let sources = import ./../../sources.nix;
+in {
+  nixpkgs.overlays = [
+    (import
+      (builtins.fetchTarball { url = sources.neovim-nightly-overlay.url; }))
+  ];
   programs = {
     vim.enable = true;
-    neovim.enable = true;
+    neovim = {
+      enable = true;
+      package = pkgs.neovim-nightly;
+    };
   };
 }
