246ff20b4b1acb2c9d1cae65dd7632b4467874fb
ttak0422 <bgdaewalkman@gmail.com>
2021-03-05 15:08:21 +0000

    fix ext cpptool

--- nix/nixos/applications/vscode/default.nix
+++ nix/nixos/applications/vscode/default.nix
@@ -8,9 +8,10 @@ let
     version = src.version;
     sha256 = src.sha256;
   };
-  extensions = (with pkgs.vscode-extensions;
-    [ ms-kubernetes-tools.vscode-kubernetes-tools ])
-    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace
+  extensions = (with pkgs.vscode-extensions; [
+    ms-kubernetes-tools.vscode-kubernetes-tools
+    ms-vscode.cpptools
+  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace
     (lib.attrsets.mapAttrsToList mkExt sources);
   vscode-with-extensions =
     pkgs.vscode-with-extensions.override { vscodeExtensions = extensions; };
