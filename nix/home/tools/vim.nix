57fe8839c0f9fbd5cea3788bcea1050eda44763c
ttak0422 <bgdaewalkman@gmail.com>
2021-03-09 15:22:40 +0000

    :construction: wip vim

--- nix/home/tools/vim.nix
+++ nix/home/tools/vim.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 
-let sources = import ./../../sources.nix;
+let
+  sources = import ./../../sources.nix;
+  indentSpace = 2;
 in {
   nixpkgs.overlays = [
     (import
@@ -11,6 +13,32 @@ in {
     neovim = {
       enable = true;
       package = pkgs.neovim-nightly;
+      vimAlias = true;
+      vimdiffAlias = true;
+      extraConfig = ''
+        set encoding=utf-8
+        scriptencoding utf-8
+        syntax enable
+        filetype plugin indent on
+        set cursorline
+        set number
+        set relativenumber
+        set virtualedit=block
+        set wildmenu
+        " search
+        set hlsearch
+        set ignorecase
+        set smartcase
+        set incsearch
+        " indent
+        set tabstop=${toString indentSpace}
+        set smartindent
+        set clipboard+=${if pkgs.stdenv.isDarwin then "unnamed" else "unnamedplus"}
+        " tab 
+        set showtabline=2
+        " statusline
+        set laststatus=2
+      '';
     };
   };
 }
