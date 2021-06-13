ad70c116767a645f3f7f1e0a9f6cd88ab5f2816d
ttak0422 <bgdaewalkman@gmail.com>
2021-06-13 22:41:16 +0900

    :+1: nixrepl

--- nix/home/tools/pet.nix
+++ nix/home/tools/pet.nix
@@ -46,6 +46,10 @@ let
       command =
         "docker run --rm -i -v $PWD:/work -w /work -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION hashicorp/terraform:0.14.8";
     }
+    {
+      description = "nix repl";
+      command = "nix repl '<nixpkgs>'";
+    }
   ];
 in {
   home.packages = [ pkgs.peco pkgs.pet ];
