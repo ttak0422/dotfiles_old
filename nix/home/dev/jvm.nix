3f07c7e2ec5177dd399fc23fe299c1cf52085b55
ttak0422 <bgdaewalkman@gmail.com>
2021-07-18 18:07:14 +0900

    add jdk8

--- nix/home/dev/jvm.nix
+++ nix/home/dev/jvm.nix
@@ -2,13 +2,13 @@
   home = {
     packages = with pkgs; [ maven ]; 
     file = {
+      "jdk/openjdk8".source = pkgs.jdk8;
       "jdk/openjdk11".source = pkgs.jdk11;
       "jdk/openjdk16".source = pkgs.jdk16;
       "jdk/scala".source = pkgs.scala;
     };
     sessionVariables = {
       PATH="$HOME/jdk/openjdk11/bin:$PATH";
-      JAVA_HOME="$HOME/jdk/openjdk11/bin";
       JAVA_HOME="$HOME/jdk/openjdk11";
     };
   };
