fb191f53c417f09bc686ddcd98f213cd69d0001c
ttak0422 <bgdaewalkman@gmail.com>
2021-07-18 14:11:57 +0900

    jdk8 -> jdk11 & jdk16

--- nix/home/dev/jvm.nix
+++ nix/home/dev/jvm.nix
@@ -1,1 +1,14 @@
-{ config, pkgs, lib, ... }: { home.packages = with pkgs; [ jdk8 maven scala ]; }
+{ config, pkgs, lib, ... }: { 
+  home = {
+    packages = with pkgs; [ maven ]; 
+    file = {
+      "jdk/openjdk11".source = pkgs.jdk11;
+      "jdk/openjdk16".source = pkgs.jdk16;
+      "jdk/scala".source = pkgs.scala;
+    };
+    sessionVariables = {
+      PATH="$HOME/jdk/openjdk11/bin:$PATH";
+      JAVA_HOME="$HOME/jdk/openjdk11/bin";
+    };
+  };
+}
