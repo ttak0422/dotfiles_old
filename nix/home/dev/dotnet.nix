63161bb60dc610326de0dfe8f589a50731f754dd
ttak0422 <bgdaewalkman@gmail.com>
2021-09-25 23:34:45 +0900

    allow dotnet-global tool

--- nix/home/dev/dotnet.nix
+++ nix/home/dev/dotnet.nix
@@ -1,3 +1,8 @@
-{ config, pkgs, lib, ... }: {
-  home.packages = with pkgs; [ dotnet-sdk_5 mono ];
+{ config, pkgs, lib, ... }:
+let dotnet-tool-path = "${config.home.homeDirectory}/.dotnet/tools";
+in {
+  home = {
+    packages = with pkgs; [ dotnet-sdk_5 mono ];
+    sessionVariables = { PATH = "$PATH:${dotnet-tool-path}"; };
+  };
 }
